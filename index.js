9c4d105deb0171af06e6d40986ea1edf66197cc7
Jeank <glimo17@gmail.com>
2023-08-11 11:57:27 -0600

    nnn

--- index.js
+++ index.js
@@ -50,13 +50,6 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const _dirname = path.resolve();
-app.use(express.static(path.join(_dirname, "/frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(_dirname, "/frontend/build/index.html"));
-});
-
 const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
